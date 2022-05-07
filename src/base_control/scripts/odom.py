b74e38039b8771ae1634f5f4ca7d830f524699bd
BryanMqz <85954509+BryanMqz@users.noreply.github.com>
2022-05-06 22:35:40 -0500

    Update odom.py

--- src/base_control/scripts/odom.py
+++ src/base_control/scripts/odom.py
@@ -18,14 +18,11 @@ wheel_circumference = wheel_radius * 2 * math.pi
 wheels_distance = 0.19
 
 class Odometry:
+    #Initial values for left wheel and right wheel
     wl=0.0
     wr=0.0
-
+# Initialization of pose in free space, composed of position and orientation.
     pose = Pose()
-    """We want to implement control and odom in the same script by the implementation 
-    of action servers and msgs, that why we have our publisher here, as future indicator"""
-    #rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    #vel = Twist()
 
     def update_wr(self, data):
         """Callback function which is called when a new message of type Float32 is
@@ -36,13 +33,15 @@ class Odometry:
         """Callback function which is called when a new message of type Float32 is
         received by the subscriber."""
         self.wl = data.data
-    
+    #
     def robot_pose(self):
+        # Converts a given set of Euler angles, to the corresponding quaternion.
         euler = euler_from_quaternion([self.pose.orientation.x, self.pose.orientation.y, self.pose.orientation.z, self.pose.orientation.w])
         yaw = euler[2]
 
         #self.pose.orientation.w = self.pose.orientation.w+wheel_radius*((self.wr-self.wl)/wheels_distance)*dt_front
         sumVel = self.wr + self.wl
+        # Calculates robot's actual pose
         self.pose.position.x = self.pose.position.x+(wheel_radius*((sumVel)/2)*dt_front*math.cos(yaw))
         self.pose.position.y = self.pose.position.y+(wheel_radius*((sumVel)/2)*dt_front*math.sin(yaw))
         if(yaw>math.pi or yaw<-math.pi):
@@ -51,11 +50,15 @@ class Odometry:
         yaw += wheel_radius*((self.wr-self.wl)/wheels_distance)*dt_front
         rospy.loginfo(yaw)
         quat = quaternion_from_euler(0, 0, yaw)
+        # Gives to pose robots orientation
         self.pose.orientation.x = quat[0]
         self.pose.orientation.y = quat[1]
         self.pose.orientation.z = quat[2]
         self.pose.orientation.w = quat[3]
 
+    # The broadTf (Broadcast Transform) function for the robot pose message 
+    # broadcasts this robot's translation and rotation, and publishes it as
+    # a transform from frame "base_origin" to frame "base_footprint".
     def broadTf(self):
         TB = TransformBroadcaster()
         TS = TransformStamped()
@@ -65,7 +68,8 @@ class Odometry:
         TS.transform.translation = Vector3(self.pose.position.x, self.pose.position.y, self.pose.position.z)
         TS.transform.rotation = self.pose.orientation
         TB.sendTransform(TS)
-
+        
+    #Function that return robot pose
     def getPose(self):
         return self.pose
     
@@ -74,11 +78,18 @@ class Odometry:
 
 
 def main():
+    #Create node of Odometry
     rospy.init_node('Odom', anonymous=True)
     rate = rospy.Rate(100)
+""" This represents an estimate of a position and velocity in free space.  
+    The pose in this message should be specified in the coordinate frame 
+    given by header.frame_id. The twist in this message should be specified 
+    in the coordinate frame given by the child_frame_id"""
     odom = Odometry()
+    # Both /wl & /wr are topics published by the puzzlebot referring to the velocity of the wheels
     rospy.Subscriber('/wl', Float32, odom.update_wl)
     rospy.Subscriber('/wr', Float32, odom.update_wr)
+    #Restart Odometry pose to initial position
     rospy.Subscriber("position/restart", Empty, odom.restart)
 
     while not rospy.is_shutdown():
